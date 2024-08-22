e787520e45cfa470c766978b11c9adce27699bba
Khoa <khoahuynh.toronto@gmail.com>
2024-08-22 11:21:46 -0400

    use rose-pin colorscheme

--- init.lua
+++ init.lua
@@ -1,38 +1,38 @@
 local options = {
-	backup = false,
-	termguicolors = true,
-	cmdheight = 2,
-	expandtab = true,
-	shiftwidth = 2,
-	softtabstop = 2,
-	tabstop = 2,
-	autoindent = true,
-	number = true,
-	clipboard = "unnamedplus",
-	conceallevel = 0,
-	fileencoding = "utf-8",
-	hlsearch = true,
-	ignorecase = true,
-	mouse = "a",
-	showtabline = 2,
-	smartcase = true,
-	smartindent = true,
-	splitbelow = true,
-	splitright = true,
-	swapfile = false,
-	updatetime = 300,
-	writebackup = false,
-	cursorline = true,
-	relativenumber = false,
-	numberwidth = 4,
-	signcolumn = "yes",
-	wrap = false,
-	scrolloff = 8,
+  backup = false,
+  termguicolors = true,
+  cmdheight = 2,
+  expandtab = true,
+  shiftwidth = 2,
+  softtabstop = 2,
+  tabstop = 2,
+  autoindent = true,
+  number = true,
+  clipboard = "unnamedplus",
+  conceallevel = 0,
+  fileencoding = "utf-8",
+  hlsearch = true,
+  ignorecase = true,
+  mouse = "a",
+  showtabline = 2,
+  smartcase = true,
+  smartindent = true,
+  splitbelow = true,
+  splitright = true,
+  swapfile = false,
+  updatetime = 300,
+  writebackup = false,
+  cursorline = true,
+  relativenumber = false,
+  numberwidth = 4,
+  signcolumn = "yes",
+  wrap = false,
+  scrolloff = 8,
 }
 
 -- set options via vimscript
 for k, v in pairs(options) do
-	vim.opt[k] = v
+  vim.opt[k] = v
 end
 
 require("config.keymaps") -- map b4 loading packages so we can use <Leader>
@@ -44,4 +44,4 @@ require("lazy-config")
 -- vim.g.gruvbox_material_enable_bold = "1"
 
 vim.g.everforest_background = "hard"
-vim.cmd([[colorscheme everforest ]])
+vim.cmd([[colorscheme rose-pine ]])
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,22 +1,26 @@
 return {
-	"nvim-lua/plenary.nvim",
-	"sainnhe/gruvbox-material", -- colorscheme
-	"sainnhe/everforest", -- colorscheme
-	"nvim-tree/nvim-web-devicons",
-	"nvim-telescope/telescope-media-files.nvim",
-	"williamboman/mason-lspconfig.nvim",
-	"hrsh7th/nvim-cmp",
-	"hrsh7th/cmp-nvim-lsp",
-	"JoosepAlviste/nvim-ts-context-commentstring", -- Comments
-	{
-		"nvim-telescope/telescope-fzf-native.nvim",
-		build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
-	},
-	{
-		"windwp/nvim-autopairs",
-		event = "InsertEnter",
-		config = true,
-		-- use opts = {} for passing setup options
-		-- this is equalent to setup({}) function
-	},
+  "nvim-lua/plenary.nvim",
+  -- colorschemes START --
+  "sainnhe/gruvbox-material",
+  "sainnhe/everforest",
+  { "rose-pine/neovim", name = "rose-pine", variant = "moon", dark_variant = "moon" },
+  -- colorschemes END --
+  "nvim-tree/nvim-web-devicons",
+  "nvim-telescope/telescope-media-files.nvim",
+  "williamboman/mason-lspconfig.nvim",
+  "hrsh7th/nvim-cmp",
+  "hrsh7th/cmp-nvim-lsp",
+  "JoosepAlviste/nvim-ts-context-commentstring", -- Comments
+  {
+    "nvim-telescope/telescope-fzf-native.nvim",
+    build =
+    "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+  },
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = true,
+    -- use opts = {} for passing setup options
+    -- this is equalent to setup({}) function
+  },
 }
