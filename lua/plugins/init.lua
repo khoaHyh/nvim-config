ced3a55a1f3fecbd3fd60fe381d600680c510a22
Khoa <khoahuynh.toronto@gmail.com>
2025-05-01 12:25:42 -0400

    cleanup and backmerge

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -56,4 +56,34 @@ return {
 		---@type render.md.UserConfig
 		opts = {},
 	},
+	"github/copilot.vim",
+	{
+		{
+			"CopilotC-Nvim/CopilotChat.nvim",
+			dependencies = {
+				{ "github/copilot.vim" },
+				{ "nvim-lua/plenary.nvim", branch = "master" },
+			},
+			build = "make tiktoken", -- Only on MacOS or Linux
+			opts = {
+				question_header = "## @khoaHyh",
+				answer_header = "## Copilot ",
+				error_header = "## Error ",
+			},
+		},
+	},
+	{
+		"ravitemer/mcphub.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- Required for Job and HTTP requests
+		},
+		-- uncomment the following line to load hub lazily
+		--cmd = "MCPHub",  -- lazy load
+		build = "npm install -g mcp-hub@latest", -- Installs required mcp-hub npm module
+		-- uncomment this if you don't want mcp-hub to be available globally or can't use -g
+		-- build = "bundled_build.lua",  -- Use this and set use_bundled_binary = true in opts  (see Advanced configuration)
+		config = function()
+			require("mcphub").setup()
+		end,
+	},
 }
