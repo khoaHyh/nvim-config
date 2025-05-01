0230c1a5aab4fe05f323cc184b1ec1c185297620
Khoa <khoahuynh.toronto@gmail.com>
2025-05-01 12:19:16 -0400

    render markdown yay

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,52 +1,59 @@
 return {
-  "nvim-lua/plenary.nvim",
-  -- colorschemes START --
-  "sainnhe/gruvbox-material",
-  "sainnhe/everforest",
-  {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    variant = "auto",
-    dark_variant = "moon",
-    styles = {
-      bold = true,
-      italic = true,
-      transparency = false
-    }
-  },
-  -- colorschemes END --
-  "nvim-tree/nvim-web-devicons",
-  "nvim-telescope/telescope-media-files.nvim",
-  "williamboman/mason-lspconfig.nvim",
-  {
-    "hrsh7th/nvim-cmp",
-    lazy = false,
-    priority = 100,
-    dependencies = {
-      "onsails/lspkind.nvim",
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-buffer",
-      { "L3MON4D3/LuaSnip", build = "make install_jsregexp" },
-      "saadparwaiz1/cmp_luasnip",
-    },
-    config = function()
-      require "config.completion"
-    end
-  },
-  "hrsh7th/cmp-nvim-lsp",
-  "JoosepAlviste/nvim-ts-context-commentstring", -- Comments
-  {
-    "nvim-telescope/telescope-fzf-native.nvim",
-    build =
-    "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
-  },
-  {
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    config = true,
-    -- use opts = {} for passing setup options
-    -- this is equalent to setup({}) function
-  },
-  "kevinhwang91/nvim-bqf"
+	"nvim-lua/plenary.nvim",
+	-- colorschemes START --
+	"sainnhe/gruvbox-material",
+	"sainnhe/everforest",
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		variant = "auto",
+		dark_variant = "moon",
+		styles = {
+			bold = true,
+			italic = true,
+			transparency = false,
+		},
+	},
+	-- colorschemes END --
+	"nvim-tree/nvim-web-devicons",
+	"nvim-telescope/telescope-media-files.nvim",
+	"williamboman/mason-lspconfig.nvim",
+	{
+		"hrsh7th/nvim-cmp",
+		lazy = false,
+		priority = 100,
+		dependencies = {
+			"onsails/lspkind.nvim",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-buffer",
+			{ "L3MON4D3/LuaSnip", build = "make install_jsregexp" },
+			"saadparwaiz1/cmp_luasnip",
+		},
+		config = function()
+			require("config.completion")
+		end,
+	},
+	"hrsh7th/cmp-nvim-lsp",
+	"JoosepAlviste/nvim-ts-context-commentstring", -- Comments
+	{
+		"nvim-telescope/telescope-fzf-native.nvim",
+		build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+	},
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+		-- use opts = {} for passing setup options
+		-- this is equalent to setup({}) function
+	},
+	"kevinhwang91/nvim-bqf",
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+		---@module 'render-markdown'
+		---@type render.md.UserConfig
+		opts = {},
+	},
 }
