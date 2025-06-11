b660043033a91140ace1a98f90c6c77a879d2119
khoaHyh <khoahuynh.toronto@gmail.com>
2025-06-11 17:40:04 -0400

    yarn global install instead of npm

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -88,7 +88,7 @@ return {
 		},
 		-- uncomment the following line to load hub lazily
 		--cmd = "MCPHub",  -- lazy load
-		build = "npm install -g mcp-hub@latest", -- Installs required mcp-hub npm module
+		build = "yarn global add mcp-hub@latest", -- Installs required mcp-hub npm module
 		-- uncomment this if you don't want mcp-hub to be available globally or can't use -g
 		-- build = "bundled_build.lua",  -- Use this and set use_bundled_binary = true in opts  (see Advanced configuration)
 		config = function()
