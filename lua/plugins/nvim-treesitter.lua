cee9f83a82ab176cf1912d41c4d6274b45cb8dac
khoaHyh <khoahuynh.toronto@gmail.com>
2026-01-06 17:55:20 -0500

    update treesitter config

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,27 +1,26 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    main = "nvim-treesitter.configs",
-    opts = {
-      ensure_installed = {
-        "apex",
-        "c",
-        "lua",
-        "vim",
-        "vimdoc",
-        "query",
-        "javascript",
-        "html",
-        "typescript",
-        "tsx",
-        "go",
-        "markdown",
-        "python",
-      },
-      sync_install = false,
-      highlight = { enable = true },
-      indent = { enable = true },
-    },
-  },
-}
+   {
+     "nvim-treesitter/nvim-treesitter",
+     build = ":TSUpdate",
+     opts = {
+       ensure_installed = {
+         "apex",
+         "c",
+         "lua",
+         "vim",
+         "vimdoc",
+         "query",
+         "javascript",
+         "html",
+         "typescript",
+         "tsx",
+         "go",
+         "markdown",
+         "python",
+       },
+       sync_install = false,
+       highlight = { enable = true },
+       indent = { enable = true },
+     },
+   },
+ }
